e0fc88dea7d5e9088685606da0dce063f5558bf4
Dylan LEPAGE <21804570@etu.unicaen.fr>
2020-11-24 12:36:10 +0100

    Modification : BasicGoal.java, BFSPlanner

--- src/planning/BFSPlanner.java
+++ src/planning/BFSPlanner.java
@@ -1,86 +1,88 @@
-//package planning;
-//
-//import representation.Variable;
-//import java.util.*;
-//
-//public class BFSPlanner implements Planner{
-//
-//    private Map<Variable, Object> etatInitial;
-//    private Map<Map<Variable,Object>,Map<Variable,Object>> father;
-//    private Map<Map<Variable,Object>,Action> plan;
-//    private Set<Action> actions;
-//    private Goal but;
-//
-//    public BFSPlanner(Map<Variable, Object> etatInitial,Map<Map<Variable,Object>,Map<Variable,Object>> father, Map<Map<Variable,Object>,Action> plan, Set<Action> actions, Goal but) {
-//        this.etatInitial = etatInitial;
-//        this.father = father;
-//        this.plan = plan;
-//        this.actions = actions;
-//        this.but = but;
-//    }
-//
-//    @Override
-//    public List<Action> plan() {
-//        return this.bfs(this.father, this.plan,  new hashMap<Map<Variable, Object>>(), new LinkedList<Map<Variable, Object>>());
-//    }
-//
-//    public List<Action> bfs(Map<Map<Variable,Object>,Map<Variable,Object>> father, Map<Map<Variable,Object>,Action> plan, Set<Map<Variable, Object>> closed, LinkedList<Map<Variable, Object>> open){
-//        Map<Variable, Object> next = new HashMap<Variable, Object>();
-//        Deque<Map<Variable, Object>> deque = new LinkedList<String>();
-//        father.put(this.getInitialeState(),null);
-//        if(this.getGoal().isSatisfiedBy(this.getInitialeState())){
-//            return new List<Action>();
-//        }else{
-//            while(!open.isEmpty()){
-//              this.etatInitial = deque.add(open);
-//              closed.add(this.etatInitial);
-//              for(Action a : this.actions){
-//                if(a.isApplicable(this.etatInitial)){
-//                  next = a.successor(this.etatInitial);
-//                }else if(!closed.contains(next) && !open.contains(next)){
-//                  father.put(next,this.etatInitial);
-//                  plan.put(next,this.action);
-//                  if(this.getGoal().isSatisfiedBy(next)){
-//                    return get_bfs_plan(father, plan, next);
-//                  }else{
-//                    open.add(next);
-//                  }
-//                }
-//              }
-//            }
-//            return null;
-//        }
-//    }
-//
-//
-//
-//
-//
-//    public Queue<Variable> get_bfs_plan(Map<Map<Variable,Object>,Map<Variable,Object>> father, Map<Map<Variable,Object>,Set<Action>> plan){
-//        Queue<Variable> bfs_plan = new PriorityQueue<Variable>();
-//        ArrayList<Queue<Variable>> c = new ArrayList<Queue<Variable>>();
-//        while(!this.but.isEmpty()){
-//          bfs_plan.add(plan.put(next));
-//          this.but.removeLast();
-//          this.but.put(father,this.but);
-//        }
-//        c = Collections.reverse(bfs_plan);
-//        return c;
-//    }
-//
-//
-//    @Override
-//    public Map<Variable, Object> getInitialeState() {
-//        return this.etatInitial;
-//    }
-//
-//    @Override
-//    public Set<Action> getActions() {
-//        return this.actions;
-//    }
-//
-//    @Override
-//    public Goal getGoal() {
-//        return this.but;
-//    }
-//}
+package planning;
+import java.util.*;
+import representation.*;
+//import solvers.*;
+
+public class BFSPlanner implements Planner{
+
+    private Map<Variable, Object> initialState;
+    private Set<Action> actions;
+    private Goal goal;
+
+    public BFSPlanner(Map<Variable, Object> initialState, Set<Action> actions, Goal goal) {
+        this.initialState = initialState;
+        this.actions = actions;
+        this.goal = goal;
+    }
+
+    @Override
+    public List<Action> plan() {
+        return bfs();
+    }
+
+    public List<Action> bfs(){
+        Map<Map<Variable, Object>, Map<Variable, Object>> father = new HashMap<Map<Variable, Object>, Map<Variable, Object>>(); 
+        Map<Map<Variable, Object>, Action> plan = new HashMap<Map<Variable, Object>, Action>();
+        Set<Map<Variable, Object>> closed = new HashSet<Map<Variable, Object>>();
+        LinkedList<Map<Variable, Object>> open = new LinkedList<Map<Variable, Object>>();
+        Map<Variable, Object> next = new HashMap<Variable, Object>();
+        Map<Variable, Object> instantiation = new HashMap<>();
+        closed.add(this.initialState);
+        open.add(initialState);
+        father.put(this.initialState, null);
+        if (this.getGoal().isSatisfiedBy(initialState)) {
+            return new LinkedList<Action>();
+        }
+        while(!open.isEmpty()){
+            instantiation = open.pollFirst();
+            open.remove(instantiation);
+            closed.add(instantiation);
+            for(Action a : this.actions){
+                if(a.isApplicable(instantiation)){
+                    next = a.successor(instantiation);
+                    if(!closed.contains(next) && !open.contains(next)){
+                        father.put(next,instantiation);
+                        plan.put(next,a);
+                        if(this.getGoal().isSatisfiedBy(next)){
+                            return get_bfs_plan(father, plan, next);
+                        }else{
+                            open.add(next);
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public List<Action> get_bfs_plan(Map<Map<Variable,Object>,Map<Variable,Object>> father,
+                                     Map<Map<Variable,Object>,Action> plan,
+                                     Map<Variable, Object> goal2){
+        List<Action> bfs_plan = new LinkedList<Action>();
+        while(goal2 != this.initialState){
+          bfs_plan.add(plan.get(goal2));
+          goal2 = father.get(goal2);
+        }
+        Collections.reverse(bfs_plan);
+        return bfs_plan;
+    }
+
+
+    @Override
+    public Map<Variable,Object> getInitialState(){
+        return this.initialState;
+    }
+
+    @Override
+    public Set<Action> getActions(){
+        return this.actions;
+    }
+
+    @Override
+    public Goal getGoal(){
+        return this.goal;
+    }
+
+
+}
