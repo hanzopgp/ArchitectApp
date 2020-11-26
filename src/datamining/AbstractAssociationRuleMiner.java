6c7a4432a1126028209fb6ed6e6fb71ecd373acf
hanzo <enzodurandcontact@gmail.com>
2020-11-26 15:37:20 +0100

    comment debug

--- src/datamining/AbstractAssociationRuleMiner.java
+++ src/datamining/AbstractAssociationRuleMiner.java
@@ -1,61 +1,61 @@
-package datamining;
-
-import dataminingtests.Item;
-import representation.BooleanVariable;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public abstract class AbstractAssociationRuleMiner implements AssociationRuleMiner {
-    protected BooleanDatabase database;
-
-    public AbstractAssociationRuleMiner(BooleanDatabase database) {
-        this.database = database;
-    }
-
-    public BooleanDatabase getDatabase() {
-        return database;
-    }
-
-    public void setDatabase(BooleanDatabase database) {
-        this.database = database;
-    }
-
-    public static float frequency(Set<BooleanVariable> variables, Set<Itemset> items){
-        float frequency = 0;
-        for(Itemset list : items) {
-            if (list.getItems().equals(variables)) {
-                frequency += list.getFrequency();
-            }
-        }
-        return frequency;
-    }
-
-    public static float confidence(Set<BooleanVariable> premise, Set<BooleanVariable> conclusion, Set<Itemset> frequent){
-        float frequency = 0;
-        Set<BooleanVariable> test = new HashSet<>();
-        test.addAll(premise); test.addAll(conclusion);
-        frequency = frequency(test, frequent);
-        float premiseFrequency = frequency(premise, frequent);
-        return frequency/premiseFrequency;
-    }
-
-    public Set<AssociationRule> extract(float frequence, float confiance){
-        Set<AssociationRule> result = new HashSet<>();
-        Apriori apriori = new Apriori(this.getDatabase());
-        Set<Itemset> list = apriori.frequentSingletons(frequence);
-        for(Itemset item : list){
-            Set<Set<BooleanVariable>> premises = BruteForceAssociationRuleMiner.allCandidatePremises(item.getItems());
-            for(Set<BooleanVariable> premise : premises){
-                Set<BooleanVariable> conclusion = new HashSet<>();
-                //Set<BooleanVariable> conclusion = item.getItems().removeAll(premise);
-                if(confidence(premise, conclusion, list) > confiance){
-                    AssociationRule rule = new AssociationRule(premise, conclusion, frequence, confiance);
-                    result.add(rule);
-                }
-            }
-        }
-
-        return result;
-    }
-}
+//package datamining;
+//
+//import dataminingtests.Item;
+//import representation.BooleanVariable;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//public abstract class AbstractAssociationRuleMiner implements AssociationRuleMiner {
+//    protected BooleanDatabase database;
+//
+//    public AbstractAssociationRuleMiner(BooleanDatabase database) {
+//        this.database = database;
+//    }
+//
+//    public BooleanDatabase getDatabase() {
+//        return database;
+//    }
+//
+//    public void setDatabase(BooleanDatabase database) {
+//        this.database = database;
+//    }
+//
+//    public static float frequency(Set<BooleanVariable> variables, Set<Itemset> items){
+//        float frequency = 0;
+//        for(Itemset list : items) {
+//            if (list.getItems().equals(variables)) {
+//                frequency += list.getFrequency();
+//            }
+//        }
+//        return frequency;
+//    }
+//
+//    public static float confidence(Set<BooleanVariable> premise, Set<BooleanVariable> conclusion, Set<Itemset> frequent){
+//        float frequency = 0;
+//        Set<BooleanVariable> test = new HashSet<>();
+//        test.addAll(premise); test.addAll(conclusion);
+//        frequency = frequency(test, frequent);
+//        float premiseFrequency = frequency(premise, frequent);
+//        return frequency/premiseFrequency;
+//    }
+//
+//    public Set<AssociationRule> extract(float frequence, float confiance){
+//        Set<AssociationRule> result = new HashSet<>();
+//        Apriori apriori = new Apriori(this.getDatabase());
+//        Set<Itemset> list = apriori.frequentSingletons(frequence);
+//        for(Itemset item : list){
+//            Set<Set<BooleanVariable>> premises = BruteForceAssociationRuleMiner.allCandidatePremises(item.getItems());
+//            for(Set<BooleanVariable> premise : premises){
+//                Set<BooleanVariable> conclusion = new HashSet<>();
+//                //Set<BooleanVariable> conclusion = item.getItems().removeAll(premise);
+//                if(confidence(premise, conclusion, list) > confiance){
+//                    AssociationRule rule = new AssociationRule(premise, conclusion, frequence, confiance);
+//                    result.add(rule);
+//                }
+//            }
+//        }
+//
+//        return result;
+//    }
+//}
--- src/datamining/BruteForceAssociationRuleMiner.java
+++ src/datamining/BruteForceAssociationRuleMiner.java
@@ -1,54 +1,54 @@
-package datamining;
-
-import dataminingtests.Item;
-import representation.BooleanVariable;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-
-public class BruteForceAssociationRuleMiner<E> extends AbstractAssociationRuleMiner{
-
-    public BruteForceAssociationRuleMiner(BooleanDatabase database) {
-        super(database);
-    }
-
-    public static Set<Set<BooleanVariable>> allCandidatePremises(Set<BooleanVariable> items){
-        Set<Set<BooleanVariable>> result = new HashSet<>();
-
-        //ON A [(A),(B),(C),(D)]
-        for(BooleanVariable v : items){
-            Set<BooleanVariable> vTemp = new HashSet<>();
-            vTemp.add(v);
-            result.add(vTemp);
-        }
-
-        ArrayList<BooleanVariable> liste = new ArrayList<>();
-        liste.addAll(items);
-
-        for(int i=0; i<liste.size();i++){
-            result = extractEnsembles(result, liste, this.getDatabase())
-        }
-
-        for(Set<BooleanVariable> list : super.getDatabase().getTransactions()){
-            if(list != null && list.size() > 1){
-                result.add(list);
-            }
-        }
-        if(result != null){
-            return result;
-        }else{
-            return null;
-        }
-
-    }
-
-    public static Set<Set<BooleanVariable>> extractEnsembles(Set<Set<BooleanVariable>> liste, ArrayList<BooleanVariable> variables, BooleanVariable variable){
-        // liste = [A,B,C,D]
-        // result = [A], [B], [C], [D], [A,B], etc ...
-        Set<Set<BooleanVariable>> result = new HashSet<>();
-        Set<Set<BooleanVariable>> tmp = new HashSet<>();
-        for(int i=variables.indexOf(v))
-        return null;
-    }
-}
+//package datamining;
+//
+//import dataminingtests.Item;
+//import representation.BooleanVariable;
+//
+//import java.util.ArrayList;
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//public class BruteForceAssociationRuleMiner<E> extends AbstractAssociationRuleMiner{
+//
+//    public BruteForceAssociationRuleMiner(BooleanDatabase database) {
+//        super(database);
+//    }
+//
+//    public static Set<Set<BooleanVariable>> allCandidatePremises(Set<BooleanVariable> items){
+//        Set<Set<BooleanVariable>> result = new HashSet<>();
+//
+//        //ON A [(A),(B),(C),(D)]
+//        for(BooleanVariable v : items){
+//            Set<BooleanVariable> vTemp = new HashSet<>();
+//            vTemp.add(v);
+//            result.add(vTemp);
+//        }
+//
+//        ArrayList<BooleanVariable> liste = new ArrayList<>();
+//        liste.addAll(items);
+//
+//        for(int i=0; i<liste.size();i++){
+//            result = extractEnsembles(result, liste, this.getDatabase());
+//        }
+//
+//        for(Set<BooleanVariable> list : super.getDatabase().getTransactions()){
+//            if(list != null && list.size() > 1){
+//                result.add(list);
+//            }
+//        }
+//        if(result != null){
+//            return result;
+//        }else{
+//            return null;
+//        }
+//
+//    }
+//
+//    public static Set<Set<BooleanVariable>> extractEnsembles(Set<Set<BooleanVariable>> liste, ArrayList<BooleanVariable> variables, BooleanVariable variable){
+//        // liste = [A,B,C,D]
+//        // result = [A], [B], [C], [D], [A,B], etc ...
+//        Set<Set<BooleanVariable>> result = new HashSet<>();
+//        Set<Set<BooleanVariable>> tmp = new HashSet<>();
+//        for(int i=variables.indexOf(v))
+//        return null;
+//    }
+//}
