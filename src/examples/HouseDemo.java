856aca9e74a5dd58c83f484c14b730c0ece8f8eb
hanzo <enzodurandcontact@gmail.com>
2020-11-30 23:38:16 +0100

    last one

--- src/examples/HouseDemo.java
+++ src/examples/HouseDemo.java
@@ -21,7 +21,7 @@ public class HouseDemo {
     public static void main(String[] args){
 
         //Choix des parametres
-        //inputParameters();
+        inputParameters();
 
         //----------- Utilisation package representation -----------
 
